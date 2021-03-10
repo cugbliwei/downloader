9cd9e5f7543d26dac0d9bf7a45185f6ffc4b205b
wei.li <wei.li@shopee.com>
2021-03-10 16:29:28 +0800

    fix

--- main.go
+++ main.go
@@ -25,57 +25,57 @@ func NewDownloader() *Downloader {
 	}
 }
 
-func (hc *Downloader) SetCrawlerName(crawlerName string) {
-	hc.crawlerName = crawlerName
+func (d *Downloader) SetCrawlerName(crawlerName string) {
+	d.crawlerName = crawlerName
 }
 
-func (hc *Downloader) SetSessionId(sessionId string) {
-	hc.sessionId = sessionId
+func (d *Downloader) SetSessionId(sessionId string) {
+	d.sessionId = sessionId
 }
 
-func (hc *Downloader) SetFiledir(filedir string) {
-	hc.filedir = filedir
+func (d *Downloader) SetFiledir(filedir string) {
+	d.filedir = filedir
 }
 
-func (hc *Downloader) SetFilename(filename string) {
-	hc.filename = filename
+func (d *Downloader) SetFilename(filename string) {
+	d.filename = filename
 }
 
-func (hc *Downloader) SetIsWrite(isWrite bool) {
-	hc.isWrite = isWrite
+func (d *Downloader) SetIsWrite(isWrite bool) {
+	d.isWrite = isWrite
 }
 
-func (hc *Downloader) SetIsMock(isMock bool) {
-	hc.isMock = isMock
+func (d *Downloader) SetIsMock(isMock bool) {
+	d.isMock = isMock
 }
 
-func (hc *Downloader) SetAfterResponseWriteFile() {
-	hc.Client = hc.Client.OnAfterResponse(func(c *resty.Client, resp *resty.Response) error {
+func (d *Downloader) SetAfterResponseWriteFile() {
+	d.Client = d.Client.OnAfterResponse(func(c *resty.Client, resp *resty.Response) error {
 		// 初始化request，以防影响下一次的请求
-		hc.Request = nil
+		d.Request = nil
 		// 当为mock请求时不用保存源码
-		if hc.isMock {
+		if d.isMock {
 			return nil
 		}
 
-		if !hc.isWrite {
-			log.Errorf("%s 未设置写入文件", hc.sessionId)
+		if !d.isWrite {
+			log.Errorf("%s 未设置写入文件", d.sessionId)
 			return nil
 		}
 
-		if hc.crawlerName == "" || hc.sessionId == "" || hc.filedir == "" || hc.filename == "" {
-			log.Errorf("%s 参数未设置，crawlerName: %s, sessionId: %s, filedir: %s, filename: %s", hc.crawlerName, hc.sessionId, hc.filedir, hc.filename)
+		if d.crawlerName == "" || d.sessionId == "" || d.filedir == "" || d.filename == "" {
+			log.Errorf("%s 参数未设置，crawlerName: %s, sessionId: %s, filedir: %s, filename: %s", d.crawlerName, d.sessionId, d.filedir, d.filename)
 		}
-		log.Infof("%s 源码写入文件: %s，内容长度：%d", hc.sessionId, hc.filename, len(resp.Body()))
-		ioutil.WriteFile(hc.filedir+hc.filename, resp.Body(), 0644)
+		log.Infof("%s 源码写入文件: %s，内容长度：%d", d.sessionId, d.filename, len(resp.Body()))
+		ioutil.WriteFile(d.filedir+d.filename, resp.Body(), 0644)
 		return nil
 	})
 }
 
-func (hc *Downloader) request(method, link string) (*resty.Response, []byte, error) {
-	if hc.isMock {
-		log.Infof("%s 从文件: %s 中获取源码", hc.sessionId, hc.filename)
-		body, err := ioutil.ReadFile(hc.filedir + hc.filename)
+func (d *Downloader) request(method, link string) (*resty.Response, []byte, error) {
+	if d.isMock {
+		log.Infof("%s 从文件: %s 中获取源码", d.sessionId, d.filename)
+		body, err := ioutil.ReadFile(d.filedir + d.filename)
 		if err != nil {
 			return nil, body, err
 		}
@@ -87,20 +87,20 @@ func (hc *Downloader) request(method, link string) (*resty.Response, []byte, err
 		return resp, body, nil
 	}
 
-	if hc.Request != nil {
-		resp, err := hc.Request.Get(link)
+	if d.Request != nil {
+		resp, err := d.Request.Get(link)
 		return resp, resp.Body(), err
 	}
-	resp, err := hc.Client.R().Execute(method, link)
+	resp, err := d.Client.R().Execute(method, link)
 	return resp, resp.Body(), err
 }
 
-func (hc *Downloader) Get(link string) (*resty.Response, []byte, error) {
-	return hc.request("GET", link)
+func (d *Downloader) Get(link string) (*resty.Response, []byte, error) {
+	return d.request("GET", link)
 }
 
-func (hc *Downloader) Post(link string) (*resty.Response, []byte, error) {
-	return hc.request("POST", link)
+func (d *Downloader) Post(link string) (*resty.Response, []byte, error) {
+	return d.request("POST", link)
 }
 
 func main() {
